ced25ca35cbc3f483965aae4f7d9f28e55584d20
Alex Pinkney <apinkney97@hotmail.com>
2017-12-16 23:36:42 +0000

    Day 14 part 2

--- aoc2017/day14.py
+++ aoc2017/day14.py
@@ -16,9 +16,39 @@ def part1():
     return ones
 
 
+def get_neighbours(i, j):
+    neighbours = []
+    if i != 0:
+        neighbours.append((i - 1, j))
+    if i != 127:
+        neighbours.append((i + 1, j))
+    if j != 0:
+        neighbours.append((i, j - 1))
+    if j != 127:
+        neighbours.append((i, j + 1))
+    return neighbours
+
+
+def flood_fill(grid, i, j, fill='0'):
+    to_expand = {(i, j)}
+    match = grid[i][j]
+    while to_expand:
+        i, j = to_expand.pop()
+        grid[i][j] = fill
+        for i2, j2 in get_neighbours(i, j):
+            if grid[i2][j2] == match:
+                to_expand.add((i2, j2))
+
+
 def part2():
-    # TODO: Flood fill algorithm
-    pass
+    grid = get_grid()
+    groups = 0
+    for i in range(128):
+        for j in range(128):
+            if grid[i][j] == '1':
+                flood_fill(grid, i, j)
+                groups += 1
+    return groups
 
 
 if __name__ == '__main__':
