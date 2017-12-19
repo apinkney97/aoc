cc0dcf156873b73f28239aaea6375c3907f7b480
Alex Pinkney <apinkney97@hotmail.com>
2017-12-19 16:34:47 +0000

    allow load_data not to strip whitespace

--- aoc2017/util.py
+++ aoc2017/util.py
@@ -1,6 +1,8 @@
 import os
 
 
-def load_data(day):
+def load_data(day, strip=True):
     with open(os.path.join('data', 'day{:02d}.data'.format(day))) as f:
-        return [l.strip() for l in f.readlines()]
+        if strip:
+            return [l.strip() for l in f.readlines()]
+        return f.readlines()
