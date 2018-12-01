387fd0f4225752cd49d4c350f508db6b0f131a4d
Alex Pinkney <apinkney97@gmail.com>
2018-12-01 14:57:11 +0000

    Day 1

---
+++ aoc2018/day01.py
@@ -0,0 +1,23 @@
+from itertools import cycle
+
+from aoc2018.util import load_data
+
+
+def part1():
+    return sum(int(d) for d in load_data(1))
+
+
+def part2():
+    curr = 0
+    seen = {0}
+
+    for val in cycle(int(d) for d in load_data(1)):
+        curr += val
+        if curr in seen:
+            return curr
+        seen.add(curr)
+
+
+if __name__ == "__main__":
+    print("Part 1: {}".format(part1()))
+    print("Part 2: {}".format(part2()))
