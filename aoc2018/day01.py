7e2f42d2c5c9414995157c631609d77dcaca80f7
Alex Pinkney <apinkney97@gmail.com>
2018-12-02 13:17:15 +0000

    Day 2

--- aoc2018/day01.py
+++ aoc2018/day01.py
@@ -19,5 +19,5 @@ def part2():
 
 
 if __name__ == "__main__":
-    print("Part 1: {}".format(part1()))
-    print("Part 2: {}".format(part2()))
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
