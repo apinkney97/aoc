28e894d2476be00abb68e6d4426ce16225fdf13b
Alex Pinkney <alex.pinkney@tessian.com>
2019-12-02 17:33:06 +0000

    Day 2

--- aoc2019/utils.py
+++ aoc2019/utils.py
@@ -1,7 +1,8 @@
 import os
+from typing import List
 
 
-def load_data(day, strip=True):
+def load_data(day: int, strip: bool = True) -> List[str]:
     with open(os.path.join("data", f"day{day:02d}.data")) as f:
         if strip:
             return [l.strip() for l in f.readlines()]
