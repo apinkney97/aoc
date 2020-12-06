09ec485ed0f9d8b43011cca0e1f3fbf259012450
Alex Pinkney <apinkney97@hotmail.com>
2020-12-06 18:50:26 +0000

    2015 day 8

---
+++ aoc2015/day08.py
@@ -0,0 +1,32 @@
+import utils
+
+DATA = utils.load_data(8)
+
+
+def part1() -> int:
+    sum_code = 0
+    sum_mem = 0
+    for line in DATA:
+        sum_code += len(line)
+        sum_mem += len(eval(line))
+
+    return sum_code - sum_mem
+
+
+def part2() -> int:
+    sum_code = 0
+    sum_escaped = 0
+    for line in DATA:
+        sum_code += len(line)
+        escaped = '"' + line.replace("\\", "\\\\").replace('"', '\\"') + '"'
+        sum_escaped += len(escaped)
+    return sum_escaped - sum_code
+
+
+def main() -> None:
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
+
+
+if __name__ == "__main__":
+    main()
