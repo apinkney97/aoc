a144524fa13b16a1906f9545ed0ff94a62537f27
Alex Pinkney <apinkney97@gmail.com>
2020-12-23 10:10:58 +0000

    utils refactor

---
+++ utils/setdeque.py
@@ -0,0 +1,49 @@
+from collections import deque
+from typing import Deque, Dict, Generic, Iterable
+
+from utils.types import T
+
+
+class SetDeque(Generic[T]):
+    def __init__(self, iterable: Iterable[T]):
+        self._dict: Dict[T, int] = {}
+        self._deque: Deque[T] = deque()
+        self.extend(iterable)
+
+    def __add_to_dict(self, item: T) -> None:
+        try:
+            self._dict[item] += 1
+        except KeyError:
+            self._dict[item] = 1
+
+    def __remove_from_dict(self, item: T) -> None:
+        if self._dict[item] == 1:
+            self._dict.pop(item)
+        else:
+            self._dict[item] -= 1
+
+    def __contains__(self, item: T) -> bool:
+        return item in self._dict
+
+    def append(self, item: T) -> None:
+        self.__add_to_dict(item)
+        self._deque.append(item)
+
+    def appendleft(self, item: T) -> None:
+        self.__add_to_dict(item)
+        self._deque.appendleft(item)
+
+    def pop(self) -> T:
+        item = self._deque.pop()
+        self.__remove_from_dict(item)
+        return item
+
+    def popleft(self) -> T:
+        item = self._deque.popleft()
+        self.__remove_from_dict(item)
+        return item
+
+    def extend(self, iterable: Iterable[T]) -> None:
+        for item in iterable:
+            self.__add_to_dict(item)
+            self._deque.append(item)
---
+++ utils/utils.py
@@ -0,0 +1,44 @@
+import os
+from functools import reduce
+from operator import mul
+from typing import Callable, List, Optional, Union
+
+from utils.types import T, TNum
+
+DEBUG = False
+
+
+def enable_logging():
+    global DEBUG
+    DEBUG = True
+
+
+def disable_logging():
+    global DEBUG
+    DEBUG = False
+
+
+def log(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
+
+
+def load_data(
+    day: int,
+    strip: bool = True,
+    *,
+    fn: Optional[Callable[[str], T]] = None,
+    example: bool = False,
+) -> Union[List[T], List[str]]:
+    suffix = "-example" if example else ""
+    with open(os.path.join("data", f"day{day:02d}{suffix}.data")) as f:
+        data = f.readlines()
+        if strip:
+            data = [line.strip() for line in data]
+        if fn is not None:
+            data = [fn(line) for line in data]
+        return data
+
+
+def product(*args: TNum) -> TNum:
+    return reduce(mul, args)
