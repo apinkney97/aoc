70897f2a9db5833d6b9c3bfef3c23c63ff0a1e6a
Alex Pinkney <apinkney97@hotmail.com>
2020-12-25 22:04:52 +0000

    2020 day 25

---
+++ aoc2020/day25.py
@@ -0,0 +1,39 @@
+import utils
+from itertools import count
+
+
+def load_data():
+    data = utils.load_data(25, fn=int)
+
+    return data
+
+
+DATA = load_data()
+
+
+def transform(subject: int, loop_size: int) -> int:
+    value = 1
+    for _ in range(loop_size):
+        value = (value * subject) % 20201227
+    return value
+
+
+def find_loop_size(expected: int) -> int:
+    value = 1
+    for i in count():
+        if value == expected:
+            return i
+        value = (value * 7) % 20201227
+
+
+def part1() -> int:
+    loop_size = find_loop_size(DATA[0])
+    return transform(DATA[1], loop_size)
+
+
+def main() -> None:
+    print(f"Part 1: {part1()}")
+
+
+if __name__ == "__main__":
+    main()
