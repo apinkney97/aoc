ae02736c07af2a86f554ead2b56b60428eba9559
Alex Pinkney <apinkney97@hotmail.com>
2022-12-02 16:34:41 +0000

    2022 days 1 and 2

---
+++ aoc2022/day01.py
@@ -0,0 +1,42 @@
+import utils
+
+
+# EXAMPLE = True
+EXAMPLE = False
+
+
+def load_data():
+    data = utils.load_data(1, example=EXAMPLE)
+    parsed = []
+    current = []
+    for row in data:
+        if not row:
+            parsed.append(current)
+            current = []
+        else:
+            current.append(int(row))
+    if current:
+        parsed.append(current)
+    return parsed
+
+
+DATA = load_data()
+
+
+def part1() -> int:
+    return max(sum(elf) for elf in DATA)
+
+
+def part2() -> int:
+    return sum(sorted(sum(elf) for elf in DATA)[-3:])
+
+
+def main() -> None:
+    with utils.timed():
+        print(f"Part 1: {part1()}")
+    with utils.timed():
+        print(f"Part 2: {part2()}")
+
+
+if __name__ == "__main__":
+    main()
