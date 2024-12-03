33b4729ac1fd49c6ef135ef999f5736edf8a7ac1
Alex Pinkney <apinkney97@gmail.com>
2024-12-03 07:02:12 +0000

    2024 day 3

---
+++ aoc/aoc2024/day03.py
@@ -0,0 +1,38 @@
+import re
+
+
+def parse_data(data):
+    return data
+
+
+def part1(data) -> int:
+    matcher = re.compile(r"mul\((\d+),(\d+)\)")
+    result = 0
+    for line in data:
+        for match in matcher.findall(line):
+            result += int(match[0]) * int(match[1])
+    return result
+
+
+def part2(data) -> int:
+    matcher = re.compile(
+        r"""
+            (mul) \( (\d+) , (\d+) \)
+            |
+            (do) \(\)
+            |
+            (don't) \(\)
+        """,
+        re.VERBOSE,
+    )
+    result = 0
+    do = True
+    for line in data:
+        for match in matcher.findall(line):
+            if match[3] == "do":
+                do = True
+            elif match[4] == "don't":
+                do = False
+            elif do and match[0] == "mul":
+                result += int(match[1]) * int(match[2])
+    return result
