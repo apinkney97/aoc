9591bffcfbb40f887e05fa6865cff623b6911351
Alex Pinkney <apinkney97@gmail.com>
2024-12-05 07:30:16 +0000

    2024 day 5

---
+++ aoc/aoc2024/day05.py
@@ -0,0 +1,62 @@
+from aoc import utils
+
+
+def parse_data(data):
+    rules, runs = utils.split_by_blank_lines(data)
+
+    rule_map = {}
+    for rule in rules:
+        before, after = rule.split("|")
+        rule_map.setdefault(int(after), set()).add(int(before))
+
+    parsed_runs = []
+    for run in runs:
+        parsed_runs.append([int(n) for n in run.split(",")])
+
+    return rule_map, parsed_runs
+
+
+def is_valid(run: list[int], rules: dict[int, set[int]]) -> bool:
+    for i, current in enumerate(run):
+        forbidden_after = rules.get(current, set())
+        for after in run[i + 1 :]:
+            if after in forbidden_after:
+                return False
+    return True
+
+
+def part1(data) -> int:
+    result = 0
+    rules, runs = data
+
+    for run in runs:
+        if is_valid(run, rules):
+            result += run[len(run) // 2]
+
+    return result
+
+
+def get_first(numbers: set[int], rules: dict[int, set[int]]) -> tuple[int, set[int]]:
+    for n in numbers:
+        forbidden_after = rules.get(n, set())
+        remaining_numbers = set(numbers)
+        remaining_numbers.remove(n)
+
+        if not remaining_numbers & forbidden_after:
+            return n, remaining_numbers
+    raise ValueError
+
+
+def part2(data) -> int:
+    result = 0
+    rules, runs = data
+
+    for run in runs:
+        if not is_valid(run, rules):
+            remaining = set(run)
+            current = 0
+            for _ in range(len(run) // 2 + 1):
+                current, remaining = get_first(remaining, rules)
+            result += current
+
+    return result
