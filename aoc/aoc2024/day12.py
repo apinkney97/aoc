793984f1a95ae640104d05d72bc500678ec7f850
Alex Pinkney <apinkney97@gmail.com>
2024-12-14 15:45:45 +0000

    2024 day 12

---
+++ aoc/aoc2024/day12.py
@@ -0,0 +1,100 @@
+from aoc.utils.coords import Coord, Vector
+
+UP = Vector(0, -1)
+LEFT = Vector(-1, 0)
+DOWN = Vector(0, 1)
+RIGHT = Vector(1, 0)
+
+DIRECTIONS = [UP, LEFT, DOWN, RIGHT]
+
+
+def parse_data(data):
+    grid = {}
+    for y, row in enumerate(data):
+        for x, cell in enumerate(row):
+            grid[Coord(x, y)] = cell
+    return grid
+
+
+def get_regions(data) -> list[set[Coord]]:
+    unvisited = set(data)
+
+    regions = []
+    while unvisited:
+        coord = unvisited.pop()
+        colour = data[coord]
+
+        this_region = {coord}
+        queue = [coord]
+
+        # print(colour, coord)
+        while queue:
+            curr = queue.pop()
+            for direction in DIRECTIONS:
+                neighbour = curr + direction
+                if neighbour in unvisited and data[neighbour] == colour:
+                    unvisited.remove(neighbour)
+                    queue.append(neighbour)
+                    this_region.add(neighbour)
+        regions.append(this_region)
+
+    return regions
+
+
+def part1(data) -> int:
+    regions = get_regions(data)
+
+    result = 0
+
+    for region in regions:
+        area = len(region)
+        perimiter = 0
+        for coord in region:
+            colour = data[coord]
+            for direction in DIRECTIONS:
+                neighbour = coord + direction
+                if data.get(neighbour) != colour:
+                    perimiter += 1
+        result += area * perimiter
+    return result
+
+
+def part2(data) -> int:
+    """
+    Number of sides == number of vertices
+
+    - Each cell has 4 vertices
+    - Each vertex can be convex or concave, or neither
+    - Convex if both neighbours are out
+    - Concave if both neighbours are in AND diagonal is out
+
+    - Convex:
+      ?A
+      AX
+
+    - Concave
+      AX
+      XX
+
+    """
+    regions = get_regions(data)
+
+    pairs = list(zip(DIRECTIONS, DIRECTIONS[1:] + DIRECTIONS[:1]))
+
+    result = 0
+    for region in regions:
+        vertices = 0
+        for coord in region:
+            for v1, v2 in pairs:
+                n1 = coord + v1
+                n2 = coord + v2
+                if n1 not in region and n2 not in region:
+                    # Convex
+                    vertices += 1
+                elif n1 in region and n2 in region and n1 + v2 not in region:
+                    # Concave
+                    vertices += 1
+        area = len(region)
+        result += area * vertices
+
+    return result
