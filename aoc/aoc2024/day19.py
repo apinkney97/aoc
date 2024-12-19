829e4d1f63782f339ed9e993ba20b51dd725e452
Alex Pinkney <apinkney97@gmail.com>
2024-12-19 21:56:59 +0000

    2024 day 19

---
+++ aoc/aoc2024/day19.py
@@ -0,0 +1,69 @@
+import functools
+
+from aoc import utils
+
+TOWELS = {}
+
+
+def parse_data(data):
+    global TOWELS
+    towels, patterns = utils.split_by_blank_lines(data)
+    TOWELS = set(towels[0].split(", "))
+    return patterns
+
+
+@functools.cache
+def string_match(pattern, start, end, towel):
+    return pattern[start:end] == towel
+
+
+def match(pattern: str) -> bool:
+    queue = [0]
+    seen = set()
+    while queue:
+        offset = queue.pop()
+        for towel in TOWELS:
+            towel_len = len(towel)
+            next_offset = offset + towel_len
+            if next_offset in seen:
+                continue
+            if string_match(pattern, offset, next_offset, towel):
+                queue.append(next_offset)
+                seen.add(next_offset)
+                if next_offset == len(pattern):
+                    return True
+
+    return False
+
+
+@functools.cache
+def match_all(pattern: str, offset: int) -> bool:
+    matches = 0
+    for towel in TOWELS:
+        towel_len = len(towel)
+        next_offset = offset + towel_len
+        if string_match(pattern, offset, next_offset, towel):
+            if next_offset == len(pattern):
+                matches += 1
+            else:
+                matches += match_all(pattern, next_offset)
+    return matches
+
+
+def part1(data) -> int:
+    patterns = data
+    result = 0
+    for pattern in patterns:
+        if match(pattern):
+            result += 1
+    return result
+
+
+def part2(data) -> int:
+    patterns = data
+    result = 0
+    for i, pattern in enumerate(patterns):
+        count = match_all(pattern, 0)
+        # print(i, count)
+        result += count
+    return result
