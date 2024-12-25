01b28dbc40c5ba6d62d7194fbbc75e74bd31e04a
Alex Pinkney <apinkney97@gmail.com>
2024-12-25 07:59:10 +0000

    2024 day 25

---
+++ aoc/aoc2024/day25.py
@@ -0,0 +1,37 @@
+from aoc import utils
+
+
+def parse_data(data):
+    keys = []
+    locks = []
+    for lines in utils.split_by_blank_lines(data):
+        rotated = utils.rotate_clockwise(lines)
+        if lines[0][0] == ".":
+            key = []
+            for line in rotated:
+                key.append(line.index(".") - 1)
+            keys.append(key)
+        else:
+            lock = []
+            for line in rotated:
+                lock.append(line[::-1].index(".") - 1)
+            locks.append(lock)
+
+    return locks, keys
+
+
+def part1(data) -> int:
+    result = 0
+    locks, keys = data
+    for lock in locks:
+        for key in keys:
+            for l_height, k_height in zip(lock, key):
+                if l_height + k_height > 5:
+                    break
+            else:
+                result += 1
+    return result
+
+
+def part2(data) -> str:
+    return "*"
